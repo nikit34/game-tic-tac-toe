056fbee95f8a89f0ccab9c05e6023251f22bedb9
nikit34 <permikov134@mail.ru>
2024-08-20 16:57:08 +0100

    Add labels for cells

--- main.py
+++ main.py
@@ -4,5 +4,6 @@
 
 if __name__ == "__main__":
     board = [['.' for _ in range(3)] for _ in range(3)]
+    print('  1 2 3')
     for i, row in enumerate(board):
-        print(' '.join(row))
+        print(str(i) + ' ' + ' '.join(row))
