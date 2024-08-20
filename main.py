2cf4bd3b5b2fd66ffc501004dfe3d8ecd88b1c52
nikit34 <permikov134@mail.ru>
2024-08-20 17:06:17 +0100

    Add loop for players step

--- main.py
+++ main.py
@@ -15,4 +15,16 @@ if __name__ == "__main__":
         player = players[step % 2]
         print("Ход игрока: " + player)
 
+        while True:
+            try:
+                row = int(input("Введите номер строки (1-3): ")) - 1
+                col = int(input("Введите номер столбца (1-3): ")) - 1
+                if 0 <= row < 3 and 0 <= col < 3:
+                    print("Игрок делает ход")
+                    break
+                else:
+                    print("[ERROR] Введите значение от 1 до 3")
+            except ValueError:
+                print("[ERROR] Введите число")
+
         step += 1
