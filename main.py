d95f815d2c3a358316206cbb849f82a0a21f0d37
nikit34 <permikov134@mail.ru>
2024-08-20 17:39:55 +0100

    Change quotes

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def check_winner(board, player):
     return False
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     board = [['.' for _ in range(3)] for _ in range(3)]
     step = 0
     players = ['O', 'X']
@@ -36,22 +36,22 @@ if __name__ == "__main__":
     while True:
         show_board(board)
         player = players[step % 2]
-        print("Ход игрока: " + player)
+        print('Ход игрока: ' + player)
 
         while True:
             try:
-                row = int(input("Введите номер строки (1-3): ")) - 1
-                col = int(input("Введите номер столбца (1-3): ")) - 1
+                row = int(input('Введите номер строки (1-3): ')) - 1
+                col = int(input('Введите номер столбца (1-3): ')) - 1
                 if 0 <= row < 3 and 0 <= col < 3:
                     if is_cell_busy(board, row, col):
-                        print("[WARNING] Эта клетка уже занята")
+                        print('[WARNING] Эта клетка уже занята')
                     else:
                         point_cell(board, row, col, player)
                         break
                 else:
-                    print("[ERROR] Введите значение от 1 до 3")
+                    print('[ERROR] Введите значение от 1 до 3')
             except ValueError:
-                print("[ERROR] Введите число")
+                print('[ERROR] Введите число')
 
         if check_winner(board, player):
             show_board(board)
