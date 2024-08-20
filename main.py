592bc8769378584a3afb6c4d740c6ac449106658
nikit34 <permikov134@mail.ru>
2024-08-20 16:59:58 +0100

    Add steps player

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def show_board(board):
 
 if __name__ == "__main__":
     board = [['.' for _ in range(3)] for _ in range(3)]
+    step = 0
+    players = ['O', 'X']
 
     while True:
         show_board(board)
+        player = players[step % 2]
+        print("Ход игрока: " + player)
