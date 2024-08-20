84c15719df513fcd4e62376aa808ff6360cc2057
nikit34 <permikov134@mail.ru>
2024-08-20 16:58:28 +0100

    Add game loop

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 
-
+def show_board(board):
+    print('  1 2 3')
+    for i, row in enumerate(board):
+        print(str(i) + ' ' + ' '.join(row))
 
 
 if __name__ == "__main__":
     board = [['.' for _ in range(3)] for _ in range(3)]
-    print('  1 2 3')
-    for i, row in enumerate(board):
-        print(str(i) + ' ' + ' '.join(row))
+
+    while True:
+        show_board(board)
